b865f560c0c2f35c57cf08f64d4ecf0c29c33a60
David Norris <x00046108@gmail.com>
2014-10-14 11:50:47 +0100

    Test case for grades.class

---
+++ x00046108-grades.sh
@@ -0,0 +1,8 @@
+java grades 49 49  #fail
+java grades 39 100 #Component Fail
+java grades 100 39 #Component Fail
+java grades 51 51 #pass
+java grades 81 81 #Pass with distinction
+java grades 101 89 #invaild input
+java grades 89 101 #invaild input
+java grades 70% 70% #invaild input
