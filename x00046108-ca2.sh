fc8bf0a8be298f333f6e7e05357c08111250d495
mint <x00046108@gmail.com>
2014-11-12 15:58:46 +0000

    Upload

--- x00046108-ca2.sh
+++ x00046108-ca2.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # Test Case 1
-# Inout: 
-# Expected Output=  
-# Actual:
+# Input: Speed Limit: 30, Speed: 101 
+# Expected Output=  "SUSPEND"
+# Actual: Noting
 
-output=$(java trafficfine 49 49)
 pass=0
 fail=0
 
-if [ $output == "Fail" ]; then
-	echo 'Test case 1 Passed'
+output=$(java trafficfine 30 101)
+
+if [[ $output == "SUSPEND" ]]; then
+	echo "Test case 1 Passed"
        	pass=$((pass + 1))
 else 
 	echo 'Test case 1 Failed'
@@ -19,13 +20,13 @@ fi
 	
 
 # Test Case 2 
-# Input: 
-# Expected 
-# Actual:
+# Input: Speed Limit: 30, Speed: 29
+# Expected: "EUR 0"
+# Actual: "EUR 0"
 
-output1=(java trafficfine 49 49)
+output1=$(java trafficfine 30 30)
 
-if [ $output1 == "Component Fail" ]; then
+if [[ $output1 == "EUR 0" ]]; then
 	echo 'Test case 2 Passed'
 	pass=$(($pass + 1))
 else
@@ -35,13 +36,13 @@ fi
 
 
 #Test Case: 3
-#Exam Score: 
-#Expected Output:
-#Actual: 
+#Input: Speed Limit: 30, Speed 35.
+#Expected Output: "EUR 80"
+#Actual: Notting
 
-output2=(java trafficfine 49 49)
+output2=$(java trafficfine 30 35)
 
-if [ $output2 == "Pass" ]; then
+if [[ $output2 == "EUR 80" ]]; then
 	echo 'Test case 3 Passed'
 	pass=$(($pass + 1))
 else
@@ -51,12 +52,12 @@ fi
 
 
 #Test Case 4
-#Exam Score: 
-#Expected Output: 
-#Actual:
+#Input: Speed Limit: 30, Speed: 50
+#Expected Output: "EUR 100"
+#Actual: Notting
 
-output3=(java trafficfine 50% 50%)
-if [ $output3 == "Invalid input" ]; then
+output3=$(java trafficfine 30 50)
+if [[ $output3 == "EUR 100" ]]; then
 	echo 'Test case 4 Passed'
 	pass=$(($pass + 1))
 else
